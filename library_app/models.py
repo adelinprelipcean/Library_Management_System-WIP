fce0bdbe2cb8023c8c723d075a00a4240f02e478
Adelin Prelipcean <97390311+adelinprelipcean@users.noreply.github.com>
2024-01-02 00:54:30 +0200

    Added login page
    -added frontend of login page and redirect to register_page

--- library_app/models.py
+++ library_app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class reader(models.Model):
-    reference_id = models.CharField(max_length=200)
-    reader_name = models.CharField(max_length=200)
-    reader_contact = models.CharField(max_length=200)
-    reader_adress = models.TextField()
-    confirmed = models.BooleanField(default=False)
+class User():
+    is_admin = models.BooleanField(default=False)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)  
+    publisher = models.CharField(max_length=100)   
+    cover = models.ImageField(upload_to='library_app/covers/', null=True, blank=True)
     
+    def __str__(self):
+        return self.title
+    
+    def delete(self, *args, **kwargs):
+        self.cover.delete()
--- library_app/views.py
+++ library_app/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from django.contrib import admin
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views import generic
+from .models import User
+from django.contrib import messages
+from django.db.models import Sum
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView, ListView
+from . import models
+import operator
+import itertools
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth import authenticate, logout
+from django.contrib import auth, messages
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
